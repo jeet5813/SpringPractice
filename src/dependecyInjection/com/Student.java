2c69a74f41fbd7618c50f95fd48b022e4378330f
jeet <js5813jeet@gmail.com>
2022-11-14 04:20:54 +0530

    setters are added

--- src/dependecyInjection/com/Student.java
+++ src/dependecyInjection/com/Student.java
@@ -2,11 +2,28 @@ package dependecyInjection.com;
 
 public class Student {
 	private String studentName;
+	private int age;
+	private String addr;
+
+	
+	public void disp() {
+	  System.out.println("Student name is : "+studentName);
+	  System.out.println("Student age is : "+age);
+	  System.out.println("Student Address is : "+addr);
+	}
+
 
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
-	public void disp() {
-	  System.out.println("Student name is : "+studentName);
+
+
+	public void setAge(String age) {
+		this.age = Integer.parseInt(age);
+	}
+
+
+	public void setAddr(String addr) {
+		this.addr = addr;
 	}
 }
